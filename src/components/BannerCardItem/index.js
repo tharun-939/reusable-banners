55da0ac672b5b8c33524cef858eb85f8a3068ace
tharun-939 <165790194+tharun-939@users.noreply.github.com>
2024-05-08 18:35:09 +0530

    Update index.js

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -4,12 +4,10 @@ const CardContainer = props => {
   const {cardObj, heading, paragraph, button} = {...props}
   const {className, headerText, description} = cardObj
   return (
-    <li>
-      <div className={`${className}`}>
+    <li className={`${className}`}>
         <h1 className={`${heading}`}>{headerText}</h1>
         <p className={`${paragraph}`}>{description}</p>
         <button className={`${button}`} type="button">Show More</button>
-      </div>
     </li>
   )
 }
